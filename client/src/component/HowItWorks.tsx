39ab154efdd41fa55db64ed621f6c4238642fdb9
AZMOMAISON <mosesaizee@outlook.com>
2020-05-12 11:00:50 -0500

    Created how it works UI

--- client/src/component/HowItWorks.tsx
+++ client/src/component/HowItWorks.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import SearchHouse from "./SearchHouse";
+import HiWorksContent from "./HiWorksContent";
 import "../styles/HowItWorks.css";
 
 const HowItWorks: React.FC = () => {
@@ -7,33 +8,7 @@ const HowItWorks: React.FC = () => {
     <section id="howitworks">
       <div className="container">
         <SearchHouse />
-        <div className="hiw-content">
-          <h4>FIND YOUR DREAM HOUSE</h4>
-          <h2 className="l-headings">How it Work</h2>
-          <span className="underline"></span>
-          <div className="articles">
-            <article>
-              <i className="fas fa-search"></i>
-              <h3> Search & Find Apartment</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.{" "}
-              </p>
-            </article>
-            <article>
-              <i className="fas fa-building"></i>
-              <h3> Find Your Room</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.{" "}
-              </p>
-            </article>
-            <article>
-              <i className="fas fa-headset"></i> <h3> Talk To Agent</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.{" "}
-              </p>
-            </article>
-          </div>
-        </div>
+        <HiWorksContent />
       </div>
     </section>
   );
