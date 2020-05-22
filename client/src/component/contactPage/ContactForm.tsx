1f3eac21eba4b3013158e93fe272146237022644
AZMOMAISON <mosesaizee@outlook.com>
2020-05-22 08:38:08 -0500

    Made changes to the Login and Register Form, the NavBar, and created the Contact Page

--- client/src/component/contactPage/ContactForm.tsx
+++ client/src/component/contactPage/ContactForm.tsx
@@ -1,7 +1,34 @@
-import * as React from "react";
+import React from "react";
+import "../../styles/contact/ContactForm.css";
 
 const ContactForm: React.FC = () => {
-  return <h1>CONTACT FORM</h1>;
+  return (
+    <div className="contact-form">
+      <form>
+        <div className="form-group">
+          <label htmlFor="firstname">First Name</label>
+          <input type="text" id="firstname" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastname">Last Name</label>
+          <input type="text" id="lastname" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">E-mail address</label>
+          <input type="email" id="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone</label>
+          <input type="text" id="phone" required />
+        </div>
+        <div className="form-group full">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message"></textarea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
--- client/src/component/contactPage/TopPage.tsx
+++ client/src/component/contactPage/TopPage.tsx
@@ -1,7 +1,44 @@
-import * as React from "react";
+import React, { useState } from "react";
+import cx from "classnames";
+import { CSSTransition } from "react-transition-group";
+import "../../styles/contact/TopPage.css";
 
 const TopPage: React.FC = () => {
-  return <h1>TopPage</h1>;
+  const [display, setDisplay] = useState(false);
+
+  const handleClick = () => {
+    setDisplay(!display);
+  };
+
+  return (
+    <div className="toppage">
+      <div className="left">
+        <h2 className="l-headings">Get in touch</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque quis eum aperiam harum deserunt similique
+          architecto pariatur veniam voluptatum assumenda, earum repudiandae recusandae illo reiciendis. Earum adipisci
+          id expedita magnam.
+        </p>
+      </div>
+      <div className="right">
+        <div
+          onClick={handleClick}
+          className={cx("toggler", {
+            "toggler--active": display
+          })}
+        >
+          We are here <br /> <i className="fas fa-map-marker-alt"></i>
+        </div>
+        <CSSTransition in={display} timeout={350} classNames="display" unmountOnExit appear>
+          <div className="address-box">
+            <p> 9, Rumukrusi Street, Portharcourt, Rivers, NG</p>
+            <p>+234-(801)-111-1111</p>
+            <p>test@azmohomes.com</p>
+          </div>
+        </CSSTransition>
+      </div>
+    </div>
+  );
 };
 
 export default TopPage;
