fbd005e50aa62c42d5e82bd1e144a2ba27914338
AZMOMAISON <mosesaizee@outlook.com>
2020-06-01 09:12:20 -0500

    Created the filterBox for the property page

--- client/src/component/propertiesPage/FilterBox.tsx
+++ client/src/component/propertiesPage/FilterBox.tsx
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../styles/properties/FilterBox.css";
 
 const FilterBox = () => {
+  const [modal, showModal] = useState(false);
+
+  const handleModal = () => {
+    showModal(!modal);
+  };
+
   return (
-    <form>
-      <input type="text" />
-      <select name="type" id="">
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-      </select>
-    </form>
+    <div className="filterbox">
+      <form className="search-input">
+        <input type="text" placeholder="Address, city or state" />
+        <button type="submit">
+          <i className="fas fa-search"></i>
+        </button>
+      </form>
+      <button onClick={handleModal}>
+        Price <i className="fas fa-sort-down"></i>
+      </button>
+      {modal && (
+        <div className="menu-list">
+          <ul>
+            <li>Lorem</li>
+            <li>Lorem</li>
+            <li>Lorem</li>
+          </ul>
+        </div>
+      )}
+      {/* <li className="dropdown-menu">
+          <button>
+            Bed&Bath <i className="fas fa-sort-down"></i>
+          </button>
+          <div className="menu-list">
+            <ul>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+            </ul>
+          </div>
+        </li>
+        <li className="dropdown-menu">
+          <button>
+            Home type <i className="fas fa-sort-down"></i>
+          </button>
+          <div className="menu-list">
+            <ul>
+              <li>Lorem</li>
+              <li>Lorem</li>
+              <li>Lorem</li>
+            </ul>
+          </div>
+        </li> */}
+    </div>
   );
 };
 
